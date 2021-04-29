9fce057eca1047f7a42451fbf6fecf775760196d
Amol Shende <shendeamol75@gmail.com>
2021-04-29 19:58:38 +0530

    next

--- App.js
+++ App.js
@@ -71,8 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> If you want too change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App.js</Text> This is the App.js file
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
